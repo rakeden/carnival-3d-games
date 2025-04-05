9ab46d182eaf0d22e785c5153fd250e2b96b98b8
rakeden <5896201+rakeden@users.noreply.github.com>
2025-04-05 02:18:05 +0200

    add missing public assets

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import './index.css';
-import '@/assets/preview.png';
-import '@/assets/favicon.png';
-
 
 import router from '@/router';
 import { ThemeProvider } from '@/components/ThemeProvider';
